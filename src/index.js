f611e063a9daefa58d7173eafb127bab44a40d23
Yogurt7v <egorov-2k@yandex.ru>
2024-06-25 13:54:53 +0300

    fix: подправил кнопку паузы.

--- src/index.js
+++ src/index.js
@@ -7,70 +7,56 @@ require("./index.css");
 const summer_mp3_1 = __importDefault(require("../public/assets/sounds/summer.mp3"));
 const rain_mp3_1 = __importDefault(require("../public/assets/sounds/rain.mp3"));
 const winter_mp3_1 = __importDefault(require("../public/assets/sounds/winter.mp3"));
-const background = document.getElementsByTagName("body");
+const backgroundElement = document.getElementsByTagName("body")[0];
 const buttonsWrapper = document.getElementById("buttonsWrapper");
-const volume = document.querySelector("#volume-control");
+const volumeInput = document.querySelector("#volume-control");
 const audio = new Audio();
-let idClicked = 0;
-// function playOrPause(classname : string): void {
-//   let nested = event.target.querySelector("div")!.classList[0];
-//   if ((event.target.id !== String(idClicked)) && audio.paused) {
-//     event.target.querySelector("div")!.classList.remove("pause");
-//     event.target.querySelector("div")!.classList.add(classname);
-//     audio.play();
-//     idClicked = Number(event.target.id);
-//   } else if (Number(event.target.id) === idClicked) {
-//     audio.pause();
-//     event.target.querySelector("div")!.classList.remove(nested);
-//     event.target.querySelector("div")!.classList.add("pause");
-//     idClicked = 0;
-//   }
-// }
-function playOrPause(classname, event) {
-    const targetElement = event.target;
-    let nested = targetElement.querySelector("div").classList[0];
-    if ((targetElement.id !== String(idClicked)) && audio.paused) {
-        targetElement.querySelector("div").classList.remove("pause");
-        targetElement.querySelector("div").classList.add(classname);
+let currentButtonId = null;
+const classnames = ["sun", "rain", "snow"];
+function playOrPause(buttonClass, soundUrl, event) {
+    const clickedButton = event.target;
+    const buttonClassList = clickedButton.querySelector("div").classList;
+    if (clickedButton.id !== (currentButtonId === null || currentButtonId === void 0 ? void 0 : currentButtonId.toString())) {
+        const buttonsArray = document.querySelectorAll("button");
+        for (let i = 0; i < buttonsArray.length; i++) {
+            buttonsArray[i].querySelector("div").classList.remove("pause");
+            buttonsArray[i].querySelector("div").classList.add(classnames[i]);
+        }
+        buttonClassList.remove("pause");
+        buttonClassList.add(buttonClass);
+        audio.src = soundUrl;
         audio.play();
-        idClicked = Number(targetElement.id);
+        currentButtonId = Number(clickedButton.id);
     }
-    else if (Number(targetElement.id) === idClicked) {
+    else {
+        buttonClassList.remove(buttonClass);
+        buttonClassList.add("pause");
         audio.pause();
-        targetElement.querySelector("div").classList.remove(nested);
-        targetElement.querySelector("div").classList.add("pause");
-        idClicked = 0;
+        currentButtonId = null;
     }
 }
-function addSomeClass(classname) {
-    background[0].classList.remove("sun-background", "rain-background", "snow-background");
-    background[0].classList.add(`${classname}`);
+function updateBackground(backgroundClass) {
+    const backgroundClassList = backgroundElement.classList;
+    backgroundClassList.remove("sun-background", "rain-background", "snow-background");
+    backgroundClassList.add(backgroundClass);
 }
-volume === null || volume === void 0 ? void 0 : volume.addEventListener("change", function (e) {
-    if (e.target === null)
-        return;
-    const value = e.target;
-    audio.volume = Number(value.value) / 100;
+volumeInput === null || volumeInput === void 0 ? void 0 : volumeInput.addEventListener("change", () => {
+    audio.volume = Number(volumeInput.value) / 100;
 });
 buttonsWrapper === null || buttonsWrapper === void 0 ? void 0 : buttonsWrapper.addEventListener("click", (event) => {
-    if (event.target === null)
-        return;
-    const button = event.target;
-    switch (button.id) {
+    const clickedButton = event.target;
+    switch (clickedButton.id) {
         case "1":
-            audio.src = summer_mp3_1.default;
-            playOrPause("sun", event);
-            addSomeClass("sun-background");
+            playOrPause("sun", summer_mp3_1.default, event);
+            updateBackground("sun-background");
             break;
         case "2":
-            audio.src = rain_mp3_1.default;
-            playOrPause("rain", event);
-            addSomeClass("rain-background");
+            playOrPause("rain", rain_mp3_1.default, event);
+            updateBackground("rain-background");
             break;
         case "3":
-            audio.src = winter_mp3_1.default;
-            playOrPause("snow", event);
-            addSomeClass("snow-background");
+            playOrPause("snow", winter_mp3_1.default, event);
+            updateBackground("snow-background");
             break;
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,66 @@
 import "./index.css";
-import sunSound from "../public/assets/sounds/summer.mp3";
+import summerSound from "../public/assets/sounds/summer.mp3";
 import rainSound from "../public/assets/sounds/rain.mp3";
 import winterSound from "../public/assets/sounds/winter.mp3";
 
-const background = document.getElementsByTagName("body");
+const backgroundElement = document.getElementsByTagName("body")[0];
 const buttonsWrapper = document.getElementById("buttonsWrapper");
-const volume = document.querySelector("#volume-control");
+const volumeInput = document.querySelector<HTMLInputElement>("#volume-control");
 
 const audio = new Audio();
-let idClicked: number = 0;
+let currentButtonId: number | null = null;
+const classnames = ["sun", "rain", "snow"];
 
-function playOrPause(classname: string, event: MouseEvent): void {
-  const targetElement = event.target as Element;
-  let nested = targetElement.querySelector("div")!.classList[0];
+function playOrPause(buttonClass: string, soundUrl: string, event: MouseEvent): void {
+  const clickedButton = event.target as HTMLButtonElement;
+  const buttonClassList = clickedButton.querySelector("div")!.classList;
 
-  console.log("targetElement", targetElement.id);
-  console.log("idClicked", idClicked);
   
-
-  if ((targetElement.id !== String(idClicked)) && audio.paused) {
-    targetElement.querySelector("div")!.classList.remove("pause");
-    targetElement.querySelector("div")!.classList.add(classname);
+  if (clickedButton.id !== currentButtonId?.toString()) {
+    const buttonsArray = document.querySelectorAll("button");
+    for (let i = 0; i < buttonsArray.length; i++) {
+      buttonsArray[i].querySelector("div")!.classList.remove("pause");
+      buttonsArray[i].querySelector("div")!.classList.add(classnames[i]);
+    }
+    buttonClassList.remove("pause");
+    buttonClassList.add(buttonClass);
+    audio.src = soundUrl;
     audio.play();
-    idClicked = Number(targetElement.id);
-  } else if (Number(targetElement.id) === idClicked) {
+    currentButtonId = Number(clickedButton.id);
+  } else {
+    buttonClassList.remove(buttonClass);
+    buttonClassList.add("pause");
     audio.pause();
-    targetElement.querySelector("div")!.classList.remove(nested);
-    targetElement.querySelector("div")!.classList.add("pause");
-    idClicked = 0;
+    currentButtonId = null;
   }
 }
 
-function addSomeClass(classname: string): void {
-  background[0].classList.remove(
-    "sun-background",
-    "rain-background",
-    "snow-background"
-  );
-  background[0].classList.add(`${classname}`);
+function updateBackground(backgroundClass: string): void {
+  const backgroundClassList = backgroundElement.classList;
+  backgroundClassList.remove("sun-background", "rain-background", "snow-background");
+  backgroundClassList.add(backgroundClass);
 }
 
-volume?.addEventListener("change", function (e) {
-  if (e.target === null) return;
-  const value = (e.target as HTMLInputElement);
-  audio.volume = Number(value.value) / 100;
+volumeInput?.addEventListener("change", () => {
+  audio.volume = Number(volumeInput.value) / 100;
 });
 
 buttonsWrapper?.addEventListener("click", (event) => {
-  if (event.target === null) return
-  const button = event.target as HTMLButtonElement;
+  const clickedButton = event.target as HTMLButtonElement;
 
-  switch (button.id) {
+  switch (clickedButton.id) {
     case "1":
-      audio.src = sunSound;
-      playOrPause("sun", event);
-      addSomeClass("sun-background");
+      playOrPause("sun", summerSound, event);
+      updateBackground("sun-background");
       break;
     case "2":
-      audio.src = rainSound;
-      playOrPause("rain", event);
-      addSomeClass("rain-background");
+      playOrPause("rain", rainSound, event);
+      updateBackground("rain-background");
       break;
     case "3":
-      audio.src = winterSound;
-      playOrPause("snow", event);
-      addSomeClass("snow-background");
+      playOrPause("snow", winterSound, event);
+      updateBackground("snow-background");
       break;
   }
 });
+
